31208e666be05fd8b5053492dcb5c0c4e7849a6d
Aditya Mishra <adityakumarsmishra@gmail.com>
2024-06-29 16:17:52 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Update Copyright Year
+document.addEventListener('DOMContentLoaded', function() {
+    var currentYear = new Date().getFullYear();
+    document.getElementById('year').textContent = currentYear;
+});
+
+// this JS helps to update Your Year Automatically.
